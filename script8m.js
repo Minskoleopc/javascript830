40473c0cc1d884fe45042610b13ef961b77deef4
Minskoleopc <minskoleopc@gmail.com>
2022-10-04 08:59:31 +0530

    loops

---
+++ script8m.js
@@ -0,0 +1,94 @@
+// program -- switchcase with out break
+let city = "indore"
+switch(city){
+    case "pune":
+        console.log("MH")
+    case "indore":
+        console.log("MP")
+    case "jaipur":
+        console.log("RJ")
+    default:
+        console.log("Invalid")
+}
+
+// // program2 
+
+// let city2 = "jkl"
+// switch(city2){
+//     case "pune":
+//         console.log("MH")
+//         break;
+//     case "indore":
+//         console.log("MP")
+//         break;
+//     case "jaipur":
+//         console.log("RJ")
+//         break;
+//     default:
+//         console.log('Incorrect city name')
+// }
+
+
+// program3
+
+// let city2 = "bhopal"
+// switch(city2){
+//     case "pune":
+//     case "mumbai":
+//         console.log("MH")
+//         break;
+//     case "indore":
+//     case "bhopal":
+//         console.log("MP")
+//         break;
+//     case "jaipur":
+//     case "udaipur":
+//         console.log("RJ")
+//         break;
+//     default:
+//         console.log('Incorrect city name')
+// }
+
+// loops 
+// for loop and while loop
+
+
+// for(intialization ; conditionCheck; increment/decrement){
+
+//     console.log("hello")
+//     console.log("bye")
+
+// }
+
+for(let i = 0 ; i < 4 ; i++){ // 1 // 2 // 3 // 4
+    console.log('hello') // "hello" // "hello" // "hello" // "hello"
+}
+
+
+// print 1 to 5 using for loop
+
+for(let i = 1; i <= 5 ; i++){ // 2 // 3 // 4 // 5 // 6
+    console.log(i)  // 1 // 2 // 3 // 4 // 5
+}
+
+// print 1 to 10
+for(let j = 1 ; j <= 10 ; j++){
+    console.log(j)
+}
+// print 5 to 1
+for(let i = 5 ; i >= 1 ; i--){ // 4 // 3 // 2 // 1 // 0
+    console.log(i) // 5 //4 // 3 // 2 // 1
+}
+// table of 2
+
+for(let i = 2 ; i <= 20 ; i = i + 2){ // 4 // 6 ----------- 20 //22
+    console.log(i) // 2 // 4  //6  // 8 // 10 //12 //14 //16 //18 //20
+}
+
+
+
+
+
+
+
+
