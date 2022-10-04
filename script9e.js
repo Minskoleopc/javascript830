87f0fee500a615e578c02418b031f2d37458c653
Minskoleopc <minskoleopc@gmail.com>
2022-10-04 20:54:35 +0530

    added

---
+++ script9e.js
@@ -0,0 +1,67 @@
+console.log(1)
+console.log(2)
+console.log(3)
+console.log(4)
+console.log(5)
+
+// loop 
+// for(initialization ; conditionCheck ; increment/decrement){
+//     // statment
+// }
+
+
+for(let i = 0 ; i < 5 ; i++){ // 1 // 2 // 3 // 4 // 5
+    console.log("hello")
+    // "hello"
+    // "hello"
+    // "hello"
+    // "hello"
+    // "hello"
+}
+// print 1 to 5
+for(let i = 1 ; i <= 5 ; i++){ // 2 // 3 // 4 // 5 // 6
+    console.log(i) // 1 , 2 , 3 , 4 , 5
+}
+
+// print 1 to 10
+for(let i = 1 ; i <= 10 ; i++){
+    console.log(i)
+}
+
+// print 5 to 1
+for(let i = 5 ; i >= 1 ; i--){ // 4 // 3 // 2 // 1 //0
+    console.log(i) // 5 // 4 // 3 // 2 // 1
+}
+
+// table of 2
+
+for(let i = 2 ; i <= 20 ; i = i+2){ // 4 -------- 20 // 22
+    console.log(i) // 2//4//6/8//10//12//14//16//18//20
+}
+
+// break statement 
+for(let i = 1 ; i <= 5 ; i++){ // 2 // 3
+    if(i == 3){
+        break
+    }
+    console.log(i) //1 // 2
+}
+
+for(let i = 1 ; i <= 5 ; i++){  //2 // 3
+    console.log(i)  // 1 // 2 // 3
+    if(i == 3){
+        break
+    }
+    
+}
+
+
+
+
+
+
+
+// continue
+
+
+
