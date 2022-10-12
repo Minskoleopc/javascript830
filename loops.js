c3522eba2bcee6fa9cba78b62758093fbdc644eb
Minskoleopc <minskoleopc@gmail.com>
2022-10-12 21:01:44 +0530

    added

---
+++ loops.js
@@ -0,0 +1,152 @@
+
+// for while??
+
+// for loop 
+// for loop with break statement 
+// for loop with continue statement 
+
+
+// for(intialization ; conditionCheck ; increment/decrement){
+//     // stamement
+// }
+
+// print "hello" 5 tyms 
+
+for(let i = 0 ; i < 5 ; i++){ // i ---1 --- 2  ---- 3 ---- 4 --- 5
+    console.log("hello")
+     // "hello" 
+     // "hello"
+     // "hello"
+     // "hello"
+     // "hello"
+}
+
+// program 2
+for(let i = 0 ; i <3 ; i++){ // 1 // 2 // 3
+    console.log(i) // 0 , 1 , 2
+}
+
+// program 3
+for(let i = 1 ; i <= 5 ; i++){ 
+    console.log(i) 
+}
+
+// program 4
+// print table of 2
+for(let i = 2 ; i <= 20 ; i = i+2){ // 2 // 4 // 6 // 8 // 10 // 12 //14 // 16 // 18 // 20 // 22
+    console.log(i) // 2 // 4 // 6 // 8 // 10 // 12 //14 // 16 // 18 // 20
+}
+
+// program 5  //10 - 1
+for(let i = 10 ; i >=1 ;i--){
+    console.log(i)
+}
+
+// program 5 
+// break statement with for
+
+for(let i = 1 ; i <= 5 ; i++){ // 2 // 3
+    if(i == 3){
+        break;
+    }
+    console.log(i) // 1 // 2
+}
+
+// program6
+for(let i = 1 ; i <=  5 ; i++){ // 1 //2 // 3 // 4 // 5 // 6
+    if(i == 3){
+        continue;
+    }
+    console.log(i) // 1 // 2 // 4 // 5
+}
+
+
+// while loop
+
+// initialization
+// while(condition){
+//     // statement
+
+//     // increment
+// }
+
+let i1 = 0
+
+while(i1 < 5){
+    console.log("hello")
+    // increment/ decrement -
+    i1 ++  // 1 //2 // 3 // 4
+}
+
+let i2 = 1
+while(i2 <=5){
+    console.log(i2)
+    i2 ++
+}
+
+let i3 = 2
+while(i3 <= 20){
+    console.log(i3)
+    i3 = i3 + 2
+}
+
+let i4 = 10
+while(i4 >= 1){
+    console.log(i4)
+    i4 -- 
+}
+
+let i5 = 1
+while(i5 <= 5){
+    if(i5 == 3){
+        break
+    }
+    console.log(i5) // 1 // 2
+    i5 ++ // 2 //3
+}
+
+
+let i6 = 1
+while(i6 <= 5){
+    console.log(i6)
+    if(i6 == 3){
+        break
+    }
+    i6 ++ 
+}
+// while with continue
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// while  loop 
+// while with break statement 
+// while with continue statement
