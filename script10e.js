cf17deadfacf579c85546187cfd70a4b945fe0e0
Minskoleopc <minskoleopc@gmail.com>
2022-10-13 21:03:17 +0530

    added

---
+++ script10e.js
@@ -0,0 +1,46 @@
+
+// console.log(Math.floor(13.4)) //13
+// console.log(Math.floor(13.7)) //13
+// console.log(Math.floor(-13.7)) //-14
+
+
+// console.log(Math.ceil(24.6))
+// console.log(Math.ceil(24.2))
+// console.log(Math.ceil(-24.6)) // -24
+
+// console.log(Math.random())  /// 0 ------  1
+// console.log(Math.round(12.4)) // 12
+// console.log(Math.round(12.5)) // 13
+    
+
+
+// console.log(Math.random()) /// 0   --------- 1
+// console.log(Math.random() * 6) // 0 ------------------- 6
+// console.log(Math.floor(Math.random() * 6)+1) //
+
+// 0.34 ,1.22, 2.34 ,3.55,4.66,5.99 
+// 0 ,1,2,3,4,5
+
+
+// let random = Math.floor(Math.random()*6) + 1
+// console.log(random)
+
+// add  index.html
+// ! and press tab
+// attach js file using script tag 
+// copy the index.html path into browser
+
+
+// program 1 // Guess only 
+let randomNumber = Math.floor(Math.random()*6) + 1
+console.log(randomNumber)  //3
+let userInput = prompt('Enter any number between 1 to 6')  // 3
+if(userInput == randomNumber){
+    console.log('Guess is correct')
+}
+else {
+    console.log('Guess is incorrect')
+}
+
+
+
