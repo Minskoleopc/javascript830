c93ce82a009336d35e6eacfc5aa5b182bdc91f4e
Minskoleopc <minskoleopc@gmail.com>
2022-12-27 21:23:16 +0530

    added

--- AJAX/day11.js
+++ AJAX/day11.js
@@ -1,24 +1,73 @@
+
+
+
+// Promise.race
+// Promise.any
+// Promise.allSettled
+// Promise.all
+
+
+// function getUser(id) {
+//     return fetch(`https://reqres.in/api/users?page=${id}`)
+//         .then(function (res) {
+//             if (res.ok) {
+//                 return res.json().then(function (res) {
+//                     return res.data
+//                 })
+//             }
+//             else {
+//                 return new Error('user not found')
+//             }
+//         })
+
+// }
+// async function getUsers(id, id2, id3) {
+//     try {
+//         let data = await Promise.all([
+//             getUser(id),
+//             getUser(id2),
+//             getUser(id3)
+//         ])
+//         console.log(data)
+//     }
+//     catch (err) {
+//         console.log(err)
+//     }
+
+// }
+// getUsers(1, 2, 344)
+
+
+// Promise.race
+// share market api ----> 
+// 2 second-
+// 5 second 
+
+
+// program promise.race
+function timeOut (sec){
+    return new Promise(function(resolve,reject){
+        setTimeout(function(){
+            reject(new Error('late to response'))
+        },sec*1000)
+
+    })
+}
+
 function getUser(id) {
-    return fetch(`https://reqres.in/api/users/${id}`)
-        .then(function (res) {
-            return res.json()
-        })
-        .then(function (res) {
-            return res.data
-        })
+    return fetch(`https://reqres.in/api/users?page=${id}`)
+    .then(function(res){
+        return res.json()
+    })
+    .then(function(res){
+        return res.data
+    })     
 }
-
-
-async function getInfo(id, id2, id3) {
-    Promise.all(
-        [
-            getUser(id),
-            getUser(id2),
-            getUser(id3)
-        ]
-    )
+async function getUserInTime(id,sec){
+    let a = await Promise.race([
+        getUser(id),
+        timeOut(sec)
+    ])
+    console.log(a)
 }
-getInfo(1, 2, 3)
-
-
-
+getUserInTime(1,0.000000001)
